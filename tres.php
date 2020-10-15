c1e4d4b8c251c18750362c46bdfdc1e3c454cfb3
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-15 12:26:33 +0100

    Update at5.php, dois.php, and 3 more files...

---
+++ tres.php
@@ -0,0 +1,8 @@
+<?php
+$txt1="Eu sou o Joao e";
+$txt2="na turma sou o numero";
+$txt3="Vou fazer este modulo com";
+$Num1="15";
+$Num2="20";
+
+echo $txt1. " " .$txt2. " " .$Num1. ". " .$txt3. " " .$Num2. "Valores";
