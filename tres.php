a8f6f67ee16c7c1e2877aab797fa313be8007634
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-16 11:43:53 +0100

    Update tres.php

--- tres.php
+++ tres.php
@@ -4,5 +4,32 @@ $txt2="na turma sou o numero";
 $txt3="Vou fazer este modulo com";
 $Num1="15";
 $Num2="20";
-
 echo $txt1. " " .$txt2. " " .$Num1. ". " .$txt3. " " .$Num2. "Valores";
+echo"<br>";
+echo "<br>";
+$txt1="Eu sou o Rafael e";
+$txt2="na turma sou o numero";
+$txt3=" E a minha turma é";
+$Num1="8";
+$Num2="12";
+echo $txt1. " " .$txt2. " " .$Num1. ". " .$txt3. " " .$Num2. "I-2";
+echo"<br>";
+echo"<br>";
+   print "Os testes estão no nosso disco de rede em \\Curso\Disciplinas\Modulos\Testes\"";
+   echo"<br>";
+echo"<br>";
+print "Em PHP, a declaração de uma variável implica a precedência de um carácter especial: o carater $.";
+
+   echo"<br>";
+echo"<br>";
+print "O professor chegou perto do aluno e exclamou: “se não estudares, vais reprovar de ano!”.";
+echo"<br>";
+echo"<br>";
+$txt1="O professor";
+$txt2="chegou perto";
+$txt3 ="do aluno e";
+$txt4="exclamou: “se não";
+$txt5="estudares, vais";
+$txt6="reprovar de ano!”.";
+echo $txt1. "<br> " .$txt2. " <br>" .$txt3. " <br>" .$txt4. "<br>" .$txt5. " <br>" .$txt6;
+?>
